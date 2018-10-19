8c9b9b2ccedb29f987cc5855dd11c7ec23c51db6
rsoliva <solivarodrigo@yahoo.com>
2018-10-19 17:15:23 -0500

    Added character creation activity 3 spinners

--- app/src/main/java/csc472/depaul/edu/dungeonsndragons/CharacterList.java
+++ app/src/main/java/csc472/depaul/edu/dungeonsndragons/CharacterList.java
@@ -1,13 +1,38 @@
 package csc472.depaul.edu.dungeonsndragons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CharacterList extends AppCompatActivity {
+
+    Button newCharacter;
+    TextView dndTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_list);
+
+        newCharacter = findViewById(R.id.newCharacterButton);
+        dndTitle = findViewById(R.id.dndTitle);
+
+        newCharacter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String title = dndTitle.getText().toString();
+                createCharacterActivity();
+            }
+        });
+
+
+    }
+
+    private void createCharacterActivity(){
+        Intent intent = new Intent(this, CreationScreen.class);
+        startActivity(intent);
     }
 }
